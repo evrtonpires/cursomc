0bd78499233faadf5d8ee227c806f67d277799f9
Everton Pires <epe.masork@gmail.com>
2020-03-07 12:23:51 -0300

    Testando Classe de Dominio - Categoria

--- src/main/java/com/pireseverton/resource/CategoriaResource.java
+++ src/main/java/com/pireseverton/resource/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.pireseverton.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pireseverton.domain.Categoria;
+
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
+
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest esta funcionando";
+	public List<Categoria> listar() {
+
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritório");
+
+		List<Categoria> list = new ArrayList<>();
+
+		list.add(cat1);
+		list.add(cat2);
+		return list;
 	}
 
 }
